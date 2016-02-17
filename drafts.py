8d01875cc90f6afcf0259212d2b817da15f0d623
none23 <none23>
2016-02-17 20:47:25 +0300

    about 90]% done

--- drafts.py
+++ drafts.py
@@ -5,19 +5,19 @@ import pickle
 
 # Packages {{{ }}}
 # Define params {{{ }}}
-dump_file = '/home/n/.todo'
 
 
-def load_data():
-    with open(dump_file, 'rb') as data_file:
-        all_tasks, done_tasks, failed_tasks = pickle.load(data_file)
+def load_data(a_file):
+    with open(a_file, 'rb') as data_file:
+        the_data = pickle.load(data_file)
+    return the_data
 
 
-def save_data():
+def save_data(a_file):
     global all_tasks
     global done_tasks
     global failed_tasks
-    with open(dump_file, 'wb') as data_file:
+    with open(a_file, 'wb') as data_file:
         the_data = [all_tasks, done_tasks, failed_tasks]
         pickle.dump(the_data, data_file)
 
@@ -26,7 +26,7 @@ class TaskItem:
 
     def __init__(self, essence, important=False):
         self.active = True
-        # seconds since writing this (an arbitrary value that never repeats)
+        # unique id of a class instance (=seconds since writing this code)
         self.id = int(time.time()) - 1455707700
         self.essence = essence
         if important:
@@ -49,13 +49,15 @@ class TaskItem:
             self.important = False
 
 
-def print_current(all_tasks=all_tasks):
+def print_current(all_tasks):
+    print('Don\'t forget to:')
     for task in all_tasks:
         if task.active:
             print(task)
+    return
 
 
-def print_past(done_tasks=done_tasks, failed_tasks=failed_tasks):
+def print_past(done_tasks, failed_tasks):
     print('Completed successfully:')
     for task in done_tasks:
         print(task)
@@ -64,6 +66,36 @@ def print_past(done_tasks=done_tasks, failed_tasks=failed_tasks):
         print(task)
 
 
-# +----------------+
-# | TODO: argparse |
-# +----------------+
+def main():
+    """prints active tasks (if present) on starting a shell (zsh/bash/...)"""
+
+    all_tasks = []
+    done_tasks = []
+    failed_tasks = []
+
+    # store data in this file
+    dump_file = '/home/n/.todo'
+
+    # read the data
+    [all_tasks, done_tasks, failed_tasks] = load_data(dump_file)
+
+    # print a todo list unless it's empty
+    if len(all_tasks) == len(done_tasks) + len(failed_tasks):
+        # no active tasks
+        return
+    elif len(all_tasks) > len(done_tasks) + len(failed_tasks):
+        # active tasks present
+        print_current(all_tasks)
+    else:
+        # error
+        print("memento.py is broken!")
+        exit(1)
+
+def add_task(...):
+    ...
+    # TODO
+
+def finish_task(task_id, failed=False, ...)
+    ...
+    # TODO
+
